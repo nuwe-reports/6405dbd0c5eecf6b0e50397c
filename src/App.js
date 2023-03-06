4702f147a85040143a77b9c8649871f1e0e18f79
qrppablo <torrespablomaximiliano@gmail.com>
2023-03-06 17:41:23 +0100

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>React counter challenge template - NUWE</h1>;
+  const [count, setCount] = useState(1);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  return (
+    <div>
+      <h1>React counter challenge solution - NUWE</h1>
+      <div>
+        <button data-testid="incrementButton" onClick={increment}>
+          +
+        </button>
+        <button data-testid="decrementButton" onClick={decrement}>
+          -
+        </button>
+      </div>
+      <div>
+        <h2 data-testid="countLabel">{count}</h2>
+      </div>
+    </div>
+  );
 }
 
 export default App;
